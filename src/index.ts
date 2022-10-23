4669d6e6d9a22a6cb4c4be74d9c85f4de78e51cd
Dennis Keil <dennis.keil10@gmail.com>
2022-10-23 17:17:44 +0200

    channels

--- src/index.ts
+++ src/index.ts
@@ -47,10 +47,10 @@ serve(async (req: Request) => {
         socket.send(JSON.stringify(events));
 
         // while (true) {
-        //     const msg = await redis.xread(['stream-lobbies', streamEventId], {
-        //         block: 1000
-        //     })
-        //     console.log(msg);
+            const msg = await redis.xread(['stream-lobbies', streamEventId], {
+                block: 1000
+            })
+            console.log(msg);
         // }
     };
 
