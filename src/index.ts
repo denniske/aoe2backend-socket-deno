fa424236f894a4d38deb4088a7745ed0b8cadb46
Dennis Keil <dennis.keil10@gmail.com>
2022-10-23 18:58:15 +0200

    channels

--- src/index.ts
+++ src/index.ts
@@ -5,35 +5,39 @@ import {sendResponse} from "./helper/util.ts";
 serve(async (req: Request) => {
     const upgrade = req.headers.get("upgrade") || "";
     if (upgrade.toLowerCase() != "websocket") {
-        const url = new URL(req.url);
+        // const url = new URL(req.url);
         // console.log('url', url);
-        const path = url.pathname;
+        // const path = url.pathname;
 
-        if (path.startsWith('/api/lobbies')) {
-            const lobbies = await redis.get('lobbies2');
-            return sendResponse(lobbies);
-        }
-
-        if (path.startsWith('/api/room/lobbies/ingest')) {
-            const channel = new BroadcastChannel("lobbies");
-
-            const json = await req.json();
-
-            channel.postMessage(JSON.stringify(json));
-            return new Response("ingested");
-        }
-
-        if (path.startsWith('/api/room/lobbyPlayers/ingest')) {
-            const channel = new BroadcastChannel("lobbies");
-
-            const json = await req.json();
-
-            channel.postMessage(JSON.stringify(json));
-            return new Response("ingested");
-        }
+        // if (path.startsWith('/api/lobbies')) {
+        //     const lobbies = await redis.get('lobbies2');
+        //     return sendResponse(lobbies);
+        // }
+        //
+        // if (path.startsWith('/api/room/lobbies/ingest')) {
+        //     const channel = new BroadcastChannel("lobbies");
+        //
+        //     const json = await req.json();
+        //
+        //     channel.postMessage(JSON.stringify(json));
+        //     return new Response("ingested");
+        // }
+        //
+        // if (path.startsWith('/api/room/lobbyPlayers/ingest')) {
+        //     const channel = new BroadcastChannel("lobbies");
+        //
+        //     const json = await req.json();
+        //
+        //     channel.postMessage(JSON.stringify(json));
+        //     return new Response("ingested");
+        // }
 
         return new Response("request isn't trying to upgrade to websocket.");
     }
+
+    const url = new URL(req.url);
+    console.log('url', url);
+    const path = url.pathname;
 
     const { socket, response } = Deno.upgradeWebSocket(req);
 
@@ -42,45 +46,40 @@ serve(async (req: Request) => {
 
         let { streamEventId, events } = JSON.parse(await redis.get('lobbies') as string);
 
-        console.log(streamEventId, events.length);
-
-        if (socket.readyState !== WebSocket.OPEN) return;
-        socket.send(JSON.stringify(events));
+        if (socket.readyState === WebSocket.OPEN) {
+            socket.send(JSON.stringify(events));
+        }
 
         while (socket.readyState === WebSocket.OPEN) {
-            const msg = await redis.xread([{key: 'stream-lobbies', xid: streamEventId}], {
-                block: 5000
-            })
-            console.log(msg);
-            if (msg.length === 0) continue;
-            const messages = msg[0].messages;
-            streamEventId = messages[messages.length-1].xid;
-            console.log('Last streamEventId', streamEventId);
-
-            const values = messages.map((message: any) => message.fieldValues.data);
-
-            for (const value of values) {
-                if (socket.readyState !== WebSocket.OPEN) return;
-                socket.send(value);
-                // console.log('value', value);
+            const msg = await redis.xread([{key: 'stream-lobbies', xid: streamEventId}], { block: 5000 })
+            if (msg.length !== 0) {
+                const messages = msg[0].messages;
+                streamEventId = messages[messages.length - 1].xid;
+                const values = messages.map((message: any) => message.fieldValues.data);
+                for (const value of values) {
+                    if (socket.readyState === WebSocket.OPEN) {
+                        socket.send(value);
+                    }
+                }
             }
         }
     };
 
-    socket.onmessage = (e) => {
-        // console.log("socket message:", e.data);
-        const data = JSON.parse(e.data);
-        console.log("socket message:", data);
-
-        if (data.type === 'ping') {
-            socket.send(JSON.stringify({
-                type: 'pong',
-            }));
-        }
-    };
 
     socket.onerror = (e) => console.log("socket errored:", e);
     socket.onclose = () => console.log("socket closed");
+
+    // socket.onmessage = (e) => {
+    //     // console.log("socket message:", e.data);
+    //     const data = JSON.parse(e.data);
+    //     console.log("socket message:", data);
+    //
+    //     if (data.type === 'ping') {
+    //         socket.send(JSON.stringify({
+    //             type: 'pong',
+    //         }));
+    //     }
+    // };
 
     // const channel = new BroadcastChannel("lobbies");
     // channel.onmessage = (e) => {
@@ -89,5 +88,6 @@ serve(async (req: Request) => {
     //         socket.send(e.data);
     //     }
     // };
+
     return response;
 });
